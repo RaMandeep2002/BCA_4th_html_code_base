42005afc0a30742df025e2aca21536948410aa87
RaMandeep2002 <ramandeepsingh1511@gmail.com>
2022-06-01 21:30:14 +0530

    some changes in css file and create another directory

--- code/program 15/js/index.js
+++ code/program 15/js/index.js
@@ -1,31 +1,36 @@
 var loader
-// here is the code for the apply button
+    // here is the code for the apply button
 
 function applyNow() {
-  alert('You want a admission in this college')
-  window.location.href = 'https://admissions.cgc.ac.in/'
+    alert('You want a admission in this college')
+    window.location.href = 'https://admissions.cgc.ac.in/'
+}
+
+function feedBack() {
+    alert('Fill Your Feedback')
+    window.location.href = 'https://admissions.cgc.ac.in/'
 }
 
 //here is the code for the loader
 function loadNow(opacity) {
-  if (opacity <= 0) {
-    displayContent()
-  } else {
-    loader.style.opacity = opacity
-    window.setTimeout(function () {
-      loadNow(opacity - 0.05)
-    }, 50)
-  }
+    if (opacity <= 0) {
+        displayContent()
+    } else {
+        loader.style.opacity = opacity
+        window.setTimeout(function() {
+            loadNow(opacity - 0.05)
+        }, 50)
+    }
 }
 
 function displayContent() {
-  loader.style.display = 'none'
-  document.getElementById('content').style.display = 'block'
+    loader.style.display = 'none'
+    document.getElementById('content').style.display = 'block'
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  loader = document.getElementById('loader')
-  loadNow(1)
+document.addEventListener('DOMContentLoaded', function() {
+    loader = document.getElementById('loader')
+    loadNow(1)
 })
 
 // function collapseMenu() {

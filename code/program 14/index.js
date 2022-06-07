c8d6a0f13de953dff115b2eb3fb006b12497c14c
RaMandeep2002 <ramandeepsingh1511@gmail.com>
2022-06-07 21:51:09 +0530

    some changes in javascript file

--- code/program 14/index.js
+++ code/program 14/index.js
@@ -1,44 +1,39 @@
 function register_form() {
-    var uname = document.register.uname;
-    var email = document.register.email;
-    var Aname = document.register.Aname;
-    var phone = document.register.phone;
-    var password = document.register.password;
-    var pasw = document.register.pasw;
+    var uname = document.register.uname
+    var email = document.register.email
+    var Aname = document.register.Aname
+    var phone = document.register.phone
+    var password = document.register.password
+    var pasw = document.register.pasw
 
     if (uname.value.length <= 0) {
-        alert('Username is required');
-        uname.focus();
-        return false;
+        alert('Username is required')
+        uname.focus()
+        return false
     }
     if (email.value.length <= 0) {
-        alert('Email is required');
-        email.focus();
-        return false;
+        alert('Email is required')
+        email.focus()
+        return false
     }
     if (Aname.value.length <= 0) {
-        alert('Address is required');
-        Aname.focus();
-        return false;
+        alert('Address is required')
+        Aname.focus()
+        return false
     }
     if (phone.value.length <= 0) {
-        alert('Phone number is required');
-        phone.focus();
-        return false;
+        alert('Phone number is required')
+        phone.focus()
+        return false
     }
     if (password.value.length <= 0) {
-        alert('Password is required');
-        password.focus();
-        return false;
+        alert('Password is required')
+        password.focus()
+        return false
     }
     if (pasw.value.length <= 0) {
-        alert('confirm password is required');
-        pasw.focus();
-        return false;
+        alert('confirm password is required')
+        pasw.focus()
+        return false
     }
-    // elif(password.value != pasw.value) {
-    //     alert('Password does not match');
-    //     pasw.focus();
-    //     return false;
-    // }
 }
